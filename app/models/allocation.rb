454981fb5c4fcbe46d564b6164ab4caa747fe584
David Thompson <dtt101@gmail.com>
2009-12-06 09:28:06 +0000

    allocations debug

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -4,7 +4,7 @@ class Allocation < ActiveRecord::Base
   belongs_to :resource
   
   validates_presence_of :resource_id, :project_id, :allocation_date
-  validates_uniqueness_of :allocation_date, :scope => [:resource_id]
+  #validates_uniqueness_of :allocation_date, :scope => [:resource_id]
   
   attr_accessor :date_from # create reader and writer methods for non-db attribute
   attr_accessor :date_to # create reader and writer methods for non-db attribute
