c5c467930d025d8d430f66452fbf3e909181c853
David Thompson <dtt101@gmail.com>
2009-12-07 21:18:21 +0000

    updated all email addresses to googlemail

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   def forgot_password(user, sent_at = Time.now)
     subject    'deploygo.com - Reset password'
     recipients user.email
-    from       'support@deploygo.com'
+    from       'deploygo@googlemail.com'
     sent_on    sent_at
     body       :user => user, :url => "http://www.deploygo.com/home/reset_password/#{user.reset_password_code}"
   end
